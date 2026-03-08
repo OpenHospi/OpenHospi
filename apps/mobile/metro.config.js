442a6a090060e77c9a618d97fc8689d3b2badc70
Ruben Talstra <14235001+rubentalstra@users.noreply.github.com>
2026-03-08 17:11:36 +0100

    feat: update metro configuration for Sentry integration and adjust Uniwind settings

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,24 +1,12 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getSentryExpoConfig } = require('@sentry/react-native/metro');
 const { withUniwindConfig } = require('uniwind/metro');
-// const { withSentryConfig } = require('@sentry/react-native/metro');
-// const { withUniwindConfig } = require('uniwind/metro');
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
-//
-// // config.resolver.sourceExts.push('sql');
-//
-// const uniwindConfig = withUniwindConfig(config, {
-//   cssEntryFile: './global.css',
-//   dtsFile: './uniwind-types.d.ts',
-// });
-//
-// module.exports = withSentryConfig(uniwindConfig);
+const config = getSentryExpoConfig(__dirname);
+
+config.resolver.sourceExts.push('sql');
 
 module.exports = withUniwindConfig(config, {
-  // relative path to your global.css file (from previous step)
   cssEntryFile: './src/global.css',
-  // (optional) path where we gonna auto-generate typings
-  // defaults to project's root
   dtsFile: './src/uniwind-types.d.ts',
 });
